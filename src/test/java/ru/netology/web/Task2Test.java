8d6797889881206edaadbd23a7b116ad3e31471d
pdmashkov <pdmashkov@rolftech.ru>
2025-01-15 13:21:46 +0300

    upd Task2Test | поправил тесты по замечаниям

--- src/test/java/ru/netology/web/Task2Test.java
+++ src/test/java/ru/netology/web/Task2Test.java
@@ -3,6 +3,7 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,12 +25,7 @@ public class Task2Test {
         $("[data-test-id='city'] input").setValue("Ка");
         $$(".menu-item__control").findBy(Condition.exactText("Казань")).click();
 
-        LocalDate date = LocalDate.now().plusDays(7);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
-
-        $(".icon_name_calendar").click();
-        $$(".calendar__day").findBy(Condition.exactText(date.format(formatter))).click();
+        String date = clickDateOnCalendarForm(60);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("+70008176751");
@@ -37,9 +33,29 @@ public class Task2Test {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         $(Selectors.byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(Selectors.byText("Встреча успешно забронирована на")).shouldBe(Condition.visible);
+        SelenideElement successMsg = $("[data-test-id='notification'] .notification__content");
+        successMsg.shouldBe(Condition.visible);
+        successMsg.shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
+    }
 
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        $(Selectors.byText(date.format(formatter1))).shouldBe(Condition.visible);
+    public String clickDateOnCalendarForm(int daysToAdd) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        LocalDate dateNow = LocalDate.now();
+        LocalDate dateForClick = LocalDate.now().plusDays(daysToAdd);
+
+        if (dateNow.getMonthValue() == dateForClick.getMonthValue()) {
+            $(".icon_name_calendar").click();
+            $$(".calendar__day").findBy(Condition.exactText(String.valueOf(dateForClick.getDayOfMonth()))).click();
+        } else {
+            $(".icon_name_calendar").click();
+            for (int i = 0; i < (dateForClick.getMonthValue() - dateNow.getMonthValue()); i++) {
+                $(".calendar__title [data-step='1']").click();
+            }
+            $(".icon_name_calendar").click();
+            $$(".calendar__day").findBy(Condition.exactText(String.valueOf(dateForClick.getDayOfMonth()))).click();
+        }
+
+        return dateForClick.format(formatter);
     }
 }

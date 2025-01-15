d22bd5bc9810bbcb7220a165d15f9a1145847971
pdmashkov <pdmashkov@rolftech.ru>
2025-01-15 13:21:38 +0300

    upd RequestCardTest | поправил тесты по замечаниям

--- src/test/java/ru/netology/web/RequestCardTest.java
+++ src/test/java/ru/netology/web/RequestCardTest.java
@@ -4,20 +4,18 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class RequestCardTest {
+    ServiceMethods service = new ServiceMethods();
+
     @BeforeEach
     public void setUp() {
         Selenide.open("http://localhost:9999");
@@ -32,10 +30,9 @@ public class RequestCardTest {
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
 
-        LocalDate date = LocalDate.now().plusDays(5);
+        String date = service.getDate(5, "dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-        dateElement.setValue(date.format(formatter));
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("+70008176751");
@@ -43,10 +40,9 @@ public class RequestCardTest {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         $(Selectors.byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(Selectors.byText("Встреча успешно забронирована на")).shouldBe(Condition.visible);
-
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        $(Selectors.byText(date.format(formatter1))).shouldBe(Condition.visible);
+        SelenideElement successMsg = $("[data-test-id='notification'] .notification__content");
+        successMsg.shouldBe(Condition.visible);
+        successMsg.shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
     }
 
     @Test
@@ -58,10 +54,9 @@ public class RequestCardTest {
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
 
-        LocalDate date = LocalDate.now().plusDays(5);
+        String date = service.getDate(50, "dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-        dateElement.setValue(date.format(formatter));
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("+70008176751");
@@ -69,8 +64,8 @@ public class RequestCardTest {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         SelenideElement errorMsg = $("[data-test-id='city'] .input__sub");
-        assertTrue(errorMsg.isDisplayed());
-        Assertions.assertEquals("Доставка в выбранный город недоступна", errorMsg.getText());
+        errorMsg.shouldBe(Condition.visible);
+        errorMsg.shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
     }
 
     @Test
@@ -82,10 +77,9 @@ public class RequestCardTest {
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
 
-        LocalDate date = LocalDate.now().plusDays(2);
+        String date = service.getDate(1, "dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-        dateElement.setValue(date.format(formatter));
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("+70008176751");
@@ -93,8 +87,8 @@ public class RequestCardTest {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         SelenideElement errorMsg = $("[data-test-id='date'] .input__sub");
-        assertTrue(errorMsg.isDisplayed());
-        Assertions.assertEquals("Заказ на выбранную дату невозможен", errorMsg.getText());
+        errorMsg.shouldBe(Condition.visible);
+        errorMsg.shouldHave(Condition.exactText("Заказ на выбранную дату невозможен"));
     }
 
     @Test
@@ -105,7 +99,10 @@ public class RequestCardTest {
 
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
-        dateElement.setValue("09 09 1947");
+
+        String date = service.getPastDate(180, "dd.MM.yyyy");
+
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("+70008176751");
@@ -113,8 +110,8 @@ public class RequestCardTest {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         SelenideElement errorMsg = $("[data-test-id='date'] .input__sub");
-        assertTrue(errorMsg.isDisplayed());
-        Assertions.assertEquals("Заказ на выбранную дату невозможен", errorMsg.getText());
+        errorMsg.shouldBe(Condition.visible);
+        errorMsg.shouldHave(Condition.exactText("Заказ на выбранную дату невозможен"));
     }
 
     @Test
@@ -126,10 +123,9 @@ public class RequestCardTest {
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
 
-        LocalDate date = LocalDate.now().plusDays(3);
+        String date = service.getDate(15, "dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-        dateElement.setValue(date.format(formatter));
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Alexander_123 =");
         $("[data-test-id='phone'] input").setValue("+70008176751");
@@ -137,8 +133,8 @@ public class RequestCardTest {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         SelenideElement errorMsg = $("[data-test-id='name'] .input__sub");
-        assertTrue(errorMsg.isDisplayed());
-        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", errorMsg.getText());
+        errorMsg.shouldBe(Condition.visible);
+        errorMsg.shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -150,10 +146,9 @@ public class RequestCardTest {
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
 
-        LocalDate date = LocalDate.now().plusDays(3);
+        String date = service.getDate(12, "dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-        dateElement.setValue(date.format(formatter));
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("70008176751123");
@@ -161,8 +156,8 @@ public class RequestCardTest {
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
         SelenideElement errorMsg = $("[data-test-id='phone'] .input__sub");
-        assertTrue(errorMsg.isDisplayed());
-        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", errorMsg.getText());
+        errorMsg.shouldBe(Condition.visible);
+        errorMsg.shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -174,16 +169,14 @@ public class RequestCardTest {
         dateElement.doubleClick();
         dateElement.sendKeys(BACK_SPACE);
 
-        LocalDate date = LocalDate.now().plusDays(5);
+        String date = service.getDate(11, "dd.MM.yyyy");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-        dateElement.setValue(date.format(formatter));
+        dateElement.setValue(date);
 
         $("[data-test-id='name'] input").setValue("Иванов-Алексей Петрович");
         $("[data-test-id='phone'] input").setValue("+70008176751");
         $$("button").findBy(Condition.exactText("Забронировать")).click();
 
-        SelenideElement errorMsg = $("[data-test-id='agreement'].input_invalid");
-        assertTrue(errorMsg.isDisplayed());
+        $("[data-test-id='agreement'].input_invalid").shouldBe(Condition.visible);
     }
 }
